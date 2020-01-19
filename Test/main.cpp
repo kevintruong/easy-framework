444958455316f8cbcc2a15c2164c23ddb98eda12
Vu Truong <kevin.truong.ds@gmail.com>
2020-01-19 22:52:21 +0700

    add linker script , for testing auto mapping debug symbol

--- Test/main.cpp
+++ Test/main.cpp
@@ -3,8 +3,11 @@
 //
 
 #include "gtest/gtest.h"
+extern char text_start;
+
 
 int main(int argc, char **argv) {
+    printf("value of text_start %x",&text_start);
     ::testing::InitGoogleTest(&argc, argv);
     return RUN_ALL_TESTS();
 }
